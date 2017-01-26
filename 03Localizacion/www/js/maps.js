a97acd725a36ab6987c1c956dc93b19eff79ab03
Juan Carlos <jc.gomezlobo@bannia.es>
2017-01-26 13:19:16 +0100

    03 Localizacion. Video 2, Mostrar un mapa desde un servidor público de mapas

--- 03Localizacion/www/js/maps.js
+++ 03Localizacion/www/js/maps.js
@@ -10,16 +10,19 @@ var app = {
 
   dispositivoListo: function() {
     //alert('dispositivoListo');
-    navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizacion);
+    navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
   },
 
-  dibujaCoordenadas: function(position){
-    //alert('dibujaCoordenadas');
-    var coordsDiv = document.querySelector('#coords');
-    coordsDiv.innerHTML = 'Latitud: ' + position.coords.latitude
-                        + '<br>Longitud: ' + position.coords.longitude;
+  pintaCoordenadasEnMapa: function(position){
+    var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
-  },
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamNnbHAiLCJhIjoiY2l5ZWJjcGY4MDB1ZzJ3bXhuNDY0YW53ZiJ9.p15BVvVgBaY4KPr1JimODw', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(miMapa);
+
+},
+
 
   errorAlSolicitarLocalizacion:function(error){
     //alert('errorAlSolicitarLocalizacion');
