31fbc28217324c733b963b71b03df3e7f114aa65
Juan Carlos Gomez-Lobo <jcglp@yahoo.es>
2017-02-03 19:49:06 +0100

    04 Sensores Video 2, uso de la librería phaser para juegos. Implementar fisicas y gestor de colisiones

--- 04Sensores/www/js/juego.js
+++ 04Sensores/www/js/juego.js
@@ -1,46 +1,105 @@
-var app = {
+var app={
   inicio: function(){
-    function onError(){
-      console.log('onError!');
-    }
-    navigator.accelerometer.watchAcceleration(this.onSuccess, onError, {frequency:1000});
+    DIAMETRO_BOLA = 50;
 
+    velocidadX = 0;
+    velocidadY = 0;
+    puntuacion = 0;
+
+    alto  = document.documentElement.clientHeight;
+    ancho = document.documentElement.clientWidth;
+
+    app.vigilaSensores();
+    app.iniciaJuego();
   },
 
-  onSuccess: function(datosAceleracion){
-    app.detectaAgitacion(datosAceleracion);
-    app.representaValores(datosAceleracion);
+  iniciaJuego: function(){
 
+    function preload() {
+      // Arrancamos el motor de físicas de phaser, ARCADE qeu es el básico
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+      game.stage.backgroundColor = '#f27d0c';
+      game.load.image('bola', 'assets/bola.png');
+
+    }
+
+    function create() {
+      scoreText = game.add.text(16, 16, puntuacion, { fontSize:'100px', fill:'#757676'});
+      bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola');
+      //Indicamos que se aplique la física sobre la bola
+      game.physics.arcade.enable(bola);
+      //Al cuerpo de la bola le indicamos que gestione las colisiones con el borde de la pantalla
+      bola.body.collideWorldBounds = true;
+      //Cuando choque genera una señal
+      bola.body.onWorldBounds = new Phaser.Signal();
+      //a la seña, le añadimos el manejador
+      bola.body.onWorldBounds.add(app.decrementaPuntuacion, this);
+
+    }
+
+    function update() {
+      bola.body.velocity.y = (velocidadY * 300);
+      bola.body.velocity.x = (velocidadX * -300);
+    }
+
+
+    var estados = { preload: preload, create: create, update: update };
+    var game = new Phaser.Game(ancho, alto, Phaser.CANVAS, 'phaser',estados);
+  },
+
+  decrementaPuntuacion: function(){
+    puntuacion = puntuacion-1;
+    scoreText.text = puntuacion;
+  },
+
+  inicioX: function(){
+    return app.numeroAleatorioHasta(ancho - DIAMETRO_BOLA );
+  },
+
+  inicioY: function(){
+    return app.numeroAleatorioHasta(alto - DIAMETRO_BOLA );
+  },
+
+  numeroAleatorioHasta: function(limite){
+    return Math.floor(Math.random() * limite);
+  },
+
+  vigilaSensores: function(){
+
+    function onError() {
+        console.log('onError!');
+    }
+
+    function onSuccess(datosAceleracion){
+      app.detectaAgitacion(datosAceleracion);
+      app.registraDireccion(datosAceleracion);
+    }
+
+    navigator.accelerometer.watchAcceleration(onSuccess, onError,{ frequency: 10 });
   },
 
   detectaAgitacion: function(datosAceleracion){
-      agitacionX = datosAceleracion.x > 10;
-      agitacionY = datosAceleracion.y > 10;
+    var agitacionX = datosAceleracion.x > 10;
+    var agitacionY = datosAceleracion.y > 10;
 
-      if (agitacionX ||  agitacionY){
-        document.body.className = 'agitado';
-      } else {
-        document.body.className = '';
-      }
+    if (agitacionX || agitacionY){
+      setTimeout(app.recomienza, 1000);
+    }
+  },
+  recomienza: function(){
+    document.location.reload(true);
+  },
+  registraDireccion: function(datosAceleracion){
+    velocidadX = datosAceleracion.x ;
+    velocidadY = datosAceleracion.y ;
+
   },
 
-  representaValores: function(datosAceleracion){
-
-    app.representa(datosAceleracion.x, '#valorx');
-    app.representa(datosAceleracion.y, '#valory');
-    app.representa(datosAceleracion.z, '#valorz');
-  },
-
-  representa: function(dato, elementoHTML){
-    redondeo = Math.round(dato * 100 ) / 100;
-    document.querySelector(elementoHTML).innerHTML = redondeo;
-  }
 
 };
 
 if ('addEventListener' in document) {
-  document.addEventListener('deviceready', function(){
-    app.inicio();
-  }, false);
-
+    document.addEventListener('deviceready', function() {
+        app.inicio();
+    }, false);
 }
